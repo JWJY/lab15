5e6f403d1d694aedb80f59debe8950c949402a7a
JWJY <warisara.jy@gmail.com>
2023-01-17 15:59:56 +0700

    completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for(int i = 1 ; i < N ; i++){
+	    cout << "Pass " <<i << ":";
+	        T a = d[i];
+	        int b = i-1;
+	        
+	while(a > d[b] and b >= 0){
+	    d[b+1] = d[b];
+	    --b;
+	}
+	d[b+1] = a;
+	
+		for(int i = 0; i < N; i++){
+		    cout << d[i] << " ";   
+		}cout << "\n";
+	}
 
 }
 
